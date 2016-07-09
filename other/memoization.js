3779272f95919628cee7ae5e9c89b45827e41987
Alster <anik123wb@gmail.com>
2016-07-09 17:51:24 +0300

    added memoization.js

---
+++ other/memoization.js
@@ -0,0 +1,23 @@
+function Memoize(fn, thisArg) {
+    let cache = []
+    return function () {
+        let key = [].join.call(arguments, ' ')
+        if (!cache.hasOwnProperty(key)) {
+            console.log(`need to ${key}?`)
+            cache[key] = fn.apply(thisArg, arguments)
+        }
+        else {
+            console.log(`${key}? again?`)
+        }
+        return cache[key]
+    }
+}
+
+function doSomeHard(a, b) {
+    return `${a}ing is done`
+}
+
+let memoized = Memoize(doSomeHard)
+console.log(memoized(`wear`, `socks`))
+console.log(memoized(`wear`, `socks`))
+console.log(memoized(`kill`, `bill`))
