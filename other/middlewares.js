8767cd68123ade3ffb8fabf32862b824c8b07ead
Alster <anik123wb@gmail.com>
2016-07-09 19:32:03 +0300

    added middlewares.js

---
+++ other/middlewares.js
@@ -0,0 +1,33 @@
+class SomePipeline {
+    constructor() {
+        this.middlewares = []
+    }
+
+    use(fn) {
+        this.middlewares.push(fn)
+    }
+
+    run(someVal) {
+        this.middlewares.forEach(fn=>fn(someVal))
+        return someVal
+    }
+}
+
+let pipeline = new SomePipeline()
+
+function checkAuthorization(user) {
+    if (user.pass === 'rightPass') user.id = 'abracadabra'
+    delete user.pass
+}
+function checkPermissions(user) {
+    if (!user.id) return;
+    if (user.id === 'abracadabra') user.permission = 'god'
+}
+
+pipeline.use(checkAuthorization)
+pipeline.use(checkPermissions)
+
+let godUser = pipeline.run({pass: 'rightPass'})
+console.log(`result is ${JSON.stringify(godUser)}`)
+let anon = pipeline.run({pass: 'wrongPass'})
+console.log(`result is ${JSON.stringify(anon)}`)
