43e036772c3e8fc053a42a5e35340c8b4797c1cd
Alster <anik123wb@gmail.com>
2016-07-10 21:49:21 +0300

    cacheAside.js add cache update time profiling

--- other/cacheAside.js
+++ other/cacheAside.js
@@ -9,37 +9,40 @@ function makeRealRequest(url) {
 }
 
 function Request(url) {
+    let cacheUpdateId = ++Request.counter
     return new Promise(resolve=> {
+        console.time('cache updated ' + cacheUpdateId)
         if (cache.hasOwnProperty(url)) resolve(cache[url])
         makeRealRequest(url).then(answer=> {
-            console.log('cache updated')
+            console.timeEnd('cache updated ' + cacheUpdateId)
             cache[url] = answer
             resolve(answer)
         })
     })
 }
+Request.counter = 0
 
 function clientRequest(url, time) {
     let requestId = ++clientRequest.counter
     return timeout(time)
-        .then(()=>console.time('request' + requestId))
+        .then(()=>console.time('request ' + requestId))
         .then(()=>Request(url))
         .then(answer=> {
-            console.timeEnd('request' + requestId)
+            console.timeEnd('request ' + requestId)
             return answer;
         })
 }
 clientRequest.counter = 0
 
 clientRequest('lol', 500)
-    .then(()=>clientRequest('lol', 0))
-    .then(()=>clientRequest('lol', 0))
+    .then(()=>clientRequest('lol', 500))
+    .then(()=>clientRequest('lol', 500))
 
 /*
- cache updated
- request1: 315.012ms
- request2: 0.274ms
- request3: 0.307ms
- cache updated
- cache updated
+ cache updated 1: 316.194ms
+ request 1: 327.502ms
+ request 2: 0.449ms
+ cache updated 2: 300.396ms
+ request 3: 0.278ms
+ cache updated 3: 300.315ms
  */
