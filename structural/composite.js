dbf9bc61f8be26128586ff78549a0d88313cbe22
Alster <anik123wb@gmail.com>
2016-07-10 14:25:14 +0300

    added composite.js

---
+++ structural/composite.js
@@ -0,0 +1,50 @@
+class NodeBase {
+    constructor(name) {
+        this.name = name
+    }
+
+    display(level) {
+        return level + this.name
+    }
+}
+
+class Composite extends NodeBase {
+    constructor(name) {
+        super(name)
+        this.list = []
+    }
+
+    add(node) {
+        this.list.push(node)
+    }
+
+    display(level) {
+        return `${super.display(level)}\n${this.list.map(e=>e.display(level + '--')).join(`\n`)}`
+    }
+}
+
+class Node extends NodeBase {
+    constructor(name) {
+        super(name)
+    }
+}
+
+let child = new Composite('child')
+let pets = new Composite('pets')
+pets.add(new Node('hamster'))
+child.add(pets)
+let family = new Composite('family')
+family.add(new Node('dad'))
+family.add(new Node('mom'))
+child.add(family)
+
+console.log(child.display(''))
+
+/*
+ child
+ --pets
+ ----hamster
+ --family
+ ----dad
+ ----mom
+ */
